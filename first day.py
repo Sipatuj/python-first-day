43bdbaba4649ddd5b9cf3081f8d1b603780bd156
Konstantin <Sipatuj@gmail.com>
2016-03-14 09:21:06 +0200

    first day

---
+++ first day.py
@@ -0,0 +1,12 @@
+print('Hello')
+print('This is a great program that essentially nothing robe))')
+x = input('Remind me what\"s your name? ')
+b = int(input('Oh yeah and how old are you? '))
+c = int(input("Your weight? "))
+
+print('You really ',b," years? ",x,'?','and you have the whole ',c,'balance?')
+
+luna = c/6
+
+print( "you represent that\'s how much you weigh on the moon",luna)
+print("If you called a small child it was so ",x*5)
